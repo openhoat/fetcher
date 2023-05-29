bc0d97434c913cad012529eba402c94a8c8573be
Olivier Penhoat <openhoat@gmail.com>
2023-05-29 10:02:58 +0200

    [FEAT] Add JSON payload support

--- src/fetcher/decorators/with_json_response.ts
+++ src/fetcher/decorators/with_json_response.ts
@@ -1,26 +1,37 @@
 import type {
+  FetchURL,
   JsonFetcher,
   ResponseFetcher,
-} from '../../types/utils/fetcher.d.ts'
+} from '../../types/fetcher.d.ts'
+import type { JSONValue } from '../../types/json.d.ts'
 
-export const isJson = (response: Response) =>
+export const isJsonResponse = (response: Response) =>
   response.headers.get('Content-Type')?.includes(
     'application/json',
   )
 
+export type JsonOptions = { json?: JSONValue }
+
 export const withJsonResponse = <O extends RequestInit>(
-  fetcher: ResponseFetcher<O>,
-): JsonFetcher<O> => ({
-  fetch: async <T>(url: string, options?: O) => {
-    const headers = options?.headers
+  fetcher?: ResponseFetcher<O & JsonOptions>,
+): JsonFetcher<O & JsonOptions> => ({
+  fetch: async <T>(url: FetchURL, options?: O & JsonOptions) => {
+    const headers: HeadersInit = new Headers(options?.headers)
+    headers.append('Accept', 'application/json')
+    let body = options?.body
+    if (typeof options?.json !== 'undefined') {
+      body = JSON.stringify(options.json)
+      headers.append('Content-Type', 'application/json')
+    }
     let errorResponse: undefined | Response
     try {
-      const response = await fetcher.fetch(url, {
+      const response = await (fetcher?.fetch ?? fetch)(url, {
         ...options as O,
-        headers: { ...headers, 'Accept': 'application/json' },
+        headers,
+        body,
       })
       errorResponse = response
-      if (!isJson(response)) {
+      if (!isJsonResponse(response)) {
         throw new Error('Response is not JSON')
       }
       return await response.json() as T
